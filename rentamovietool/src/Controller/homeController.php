4600c428952e79beb49637c33949747264b92c92
Sixeye <srinath@srinath.fr>
2019-10-26 12:56:32 +0200

    Home page is there

---
+++ rentamovietool/src/Controller/homeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class homeController extends AbstractController
+{
+    /**
+     * @Route("/", name="accueil")
+     */
+    public function home(){
+        return $this->render('accueil.html.twig');
+    }
+}
