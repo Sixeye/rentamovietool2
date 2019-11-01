628b70b5d8798f24ce3710f9d0de4ae0c0b65f7a
Sixeye <srinath@srinath.fr>
2019-11-01 23:51:20 +0100

    Base components and update

--- rentamovietool/src/Controller/CameraALouer.php
+++ rentamovietool/src/Controller/CameraALouer.php
@@ -12,12 +12,14 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+
 class CameraALouer extends AbstractController
 {
     /**
      * @Route("/camera_a_louer", name="location")
      */
-    public function cameraALouer(CameraRepository $repo){
+    public function cameraALouer(CameraRepository $repo)
+    {
 
         $cameras = $repo->findAll();
         return $this->render('camera_a_louer.html.twig', [
@@ -32,16 +34,22 @@ class CameraALouer extends AbstractController
      *
      * @return Response
      */
-    public function creer(Request $request, ObjectManager $manager){
+    public function creer(Request $request, ObjectManager $manager)
+    {
         $annonce = new Camera();
 
         $form = $this->createForm(CameraType::class, $annonce);
 
         $form->handleRequest($request);
 
-        if($form->isSubmitted() && $form->isValid()){
+        if ($form->isSubmitted() && $form->isValid()) {
             $manager->persist($annonce);
             $manager->flush();
+            $this->addFlash(
+                'success',
+                "L'annonce pour la caméra <strong>{$annonce->getModele()}</strong> a bien été enregistrée."
+            );
+
             return $this->redirectToRoute('annonce_show', [
                 'slug' => $annonce->getSlug()
             ]);
@@ -58,10 +66,43 @@ class CameraALouer extends AbstractController
      *
      * @return Response
      */
-    public function show(Camera $annonce){
+    public function show(Camera $annonce)
+    {
 
         return $this->render('annonce/show.html.twig', [
-            'annonce' =>$annonce
+            'annonce' => $annonce
+        ]);
+    }
+
+    /**
+     * Permet de modifier une annonce existante
+     *
+     * @Route("/annonce/{slug}/edit", name="annonce_modifier")
+     * @return Response
+     */
+    public function update(Camera $annonce, Request $request, ObjectManager $manager)
+    {
+
+        $form = $this->createForm(CameraType::class, $annonce);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($annonce);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                "Les modifications pour la caméra <strong>{$annonce->getModele()}</strong> ont bien été enregistrées."
+            );
+
+            return $this->redirectToRoute('annonce_show', [
+                'slug' => $annonce->getSlug()
+            ]);
+        }
+
+        return $this->render('annonce/modifier.html.twig', [
+            'form' => $form->createView(),
+            'annonce' => $annonce
         ]);
     }
 
